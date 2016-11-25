13aa4d4ce8d13b1ef8f888fd5ddec3c9163b7966
Diyan Rahaman <diyan.rahaman@verizon.net>
2016-11-25 16:42:36 -0500

    Second commit

---
+++ app.js
@@ -0,0 +1,74 @@
+(function () {
+	'use strict';
+
+	angular.module('NarrowItDownApp', [])
+	       .controller('NarrowItDownController', NarrowItDownController)
+	       .service('MenuSearchService', MenuSearchService)
+	       .directive('foundItems', FoundItemsDirective);
+
+	function FoundItemsDirective() {
+		var ddo = {
+			restrict: 'E',
+			templateUrl: 'foundItems.html',
+			scope: {
+				foundItems: '<',
+				onRemove: '&'
+			}
+		};
+
+		return ddo;
+	}
+
+	NarrowItDownController.$inject = ['MenuSearchService'];
+
+	function NarrowItDownController(MenuSearchService) {
+		var ctrl = this;
+
+		ctrl.searchTerm = "";
+		ctrl.found = [];
+
+		ctrl.searchMenu = function() {
+			if (ctrl.searchTerm.trim() != "") {
+				MenuSearchService.getMatchedMenuItems(ctrl.searchTerm.toLowerCase())
+				.then(function(response) {
+					ctrl.found = response;
+				});
+			} else {
+				ctrl.found = [];
+			}
+		}
+
+		ctrl.removeItem = function(itemIndex) {
+			ctrl.found.splice(itemIndex, 1);
+		}
+	}
+
+	MenuSearchService.$inject = ['$http'];
+
+	function MenuSearchService($http) {
+		var service = this;
+
+		service.getMatchedMenuItems = function(searchTerm) {
+			return $http({
+				method: "GET",
+				url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+			})
+			.then(function (result) {
+			    // process result and only keep items that match
+			    var foundItems = [];
+			    for (var i = 0; i < result.data.menu_items.length; i++) {
+			    	if (result.data.menu_items[i].description.indexOf(searchTerm) > -1) {
+			    		foundItems.push(result.data.menu_items[i]);
+			    	}
+			    }
+			    console.log("Found items: " + foundItems.length);
+			    // return processed items
+			    return foundItems;
+			})
+			.catch(function (error) {
+		      console.log(error);
+		    });
+		}
+	}
+
+}) ();
